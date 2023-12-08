a6c94476f97c490e0022649aa6289788a3958a11
Javier <angeljavier.jimenez.torres@alumnos.upm.es>
2023-12-08 20:22:24 +0100

    Config inicial

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,6 +7,7 @@ import com.exceptions.DepthException;
 public class App{
     public static void main(String[] args) throws DepthException {
         BST<Integer> bst = new BST<Integer>(10);
+
         bst.search(10);
     }
 }
