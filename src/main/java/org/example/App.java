bf0e11e05be5bd8037df7c35d00be36897ec2c15
Javier <angeljavier.jimenez.torres@alumnos.upm.es>
2023-12-09 01:20:47 +0100

    Config inicial terminada

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,10 @@
 package org.example;
 
 import com.bst.BST;
-import com.exceptions.DepthException;
-
 
 public class App{
-    public static void main(String[] args) throws DepthException {
-        BST<Integer> bst = new BST<Integer>(10);
-
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>(10);
         bst.search(10);
     }
 }
